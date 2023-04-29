8aa1f8b80ec9b8163bce5c0727b19bd452f52dda
Mikhail <misha.shabalin@mail.ru>
2023-04-29 16:24:53 +0300

    update port in test

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ from requests import request
 
 class MyTestCase(unittest.TestCase):
     def test_200(self):
-        response = requests.request('GET', 'http://localhost:1234/')
+        response = requests.request('GET', 'http://localhost:1675/')
         sample = response.content.decode()
         self.assertEqual(sample, 'Home page')  # add assertion here
 
@@ -19,7 +19,7 @@ class MyTestCase(unittest.TestCase):
 
         with buffer as buf:
             buffer.seek(0)
-            response = request('POST', 'http://localhost:1234/classify', data=buf)
+            response = request('POST', 'http://localhost:1675/classify', data=buf)
 
         out = response.content.decode('utf-8')
         expected = 'келпи, Пембрук, Немецкая овчарка'
